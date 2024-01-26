14378d6fa9c7cc62da6895cd5cd7209a8a8e0824
Adriano Weverton <adriwco@gmail.com>
2024-01-26 00:02:59 -0300

    layout historico

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -42,119 +42,120 @@ export default function Checkout() {
     <>
       <header>
         <Navbar />
-        <div className="container header-content">
-          <h1>Finalizar Pedido</h1>
-
-          <form action="/submit" method="post" className="finalizar-section">
-            <section>
-              <h2>Dados Pessoais</h2>
-              <div className="finalizar-label-input">
-                <label htmlFor="nome">Nome Completo</label>
-                <input
-                  type="text"
-                  name="nome"
-                  id="nome"
-                  placeholder="Evite Abreviações..."
-                />
-              </div>
-              <div className="finalizar-label-input">
-                <label htmlFor="eMail">E-mail</label>
-                <input
-                  type="email"
-                  name="eMail"
-                  id="eMail"
-                  placeholder="usuario@teste.com"
-                />
-              </div>
-              <div className="finalizar-label-input">
-                <label htmlFor="telefone">Telefone Contato</label>
-                <input
-                  type="tel"
-                  name="telefone"
-                  id="telefone"
-                  placeholder="(00) 00000-0000"
-                />
-              </div>
-            </section>
-            <section>
-              <h2>Endereço de Entrega</h2>
-              <div className="finalizar-label-input">
-                <label htmlFor="cep">CEP</label>
-                <input
-                  type="number"
-                  name="cep"
-                  id="cep"
-                  placeholder="00000-000"
-                />
-              </div>
-              <div className="finalizar-label-input">
-                <label htmlFor="endereco">Endereço</label>
-                <input
-                  type="text"
-                  name="endereco"
-                  id="endereco"
-                  placeholder="Logradouro, nº..."
-                />
-              </div>
-              <div className="label-input-especial">
-                <div className="finalizar-label-input">
-                  <label htmlFor="complemento">Complemento</label>
-                  <input
-                    type="text"
-                    name="complemento"
-                    id="complemento"
-                    placeholder="Apto 000..."
-                  />
-                </div>
-                <div className="finalizar-label-input">
-                  <label htmlFor="bairro">Bairro</label>
-                  <input
-                    type="text"
-                    name="bairro"
-                    id="bairro"
-                    placeholder="Meireles..."
-                  />
-                </div>
-                <div className="finalizar-label-input">
-                  <label htmlFor="cidade">Cidade</label>
-                  <input
-                    type="text"
-                    name="cidade"
-                    id="cidade"
-                    placeholder="Fortaleza..."
-                  />
-                </div>
-                <div className="finalizar-label-input">
-                  <label htmlFor="estadoSelect">UF</label>
-                  <select
-                    id="estadoSelect"
-                    name="estadoSelect"
-                    value={estadoSelecionado}
-                    onChange={handleChange}
-                  >
-                    <option value="">Selecione um Estado</option>
-                    {estadosBrasil.map((estado, index) => (
-                      <option key={index} value={estado.abreviacao}>
-                        {estado.nome}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-              </div>
-            </section>
-            <section>
-              <h2 className="checkout-finalizar-titulo">Valores</h2>
-              <div className="checkout-finalizar">
-                <span>Total</span>
-                <strong>R$ 50,00</strong>
-                <button>Finalizar Pedido</button>
-              </div>
-            </section>
-          </form>
-        </div>
-
-        <section aria-label="Lista de Produtos Disponíveis"></section>
       </header>
+
+      <main className="container header-content">
+        <h1>Finalizar Pedido</h1>
+
+        <form action="/submit" method="post" className="finalizar-section">
+          <section>
+            <h2>Dados Pessoais</h2>
+            <div className="finalizar-label-input">
+              <label htmlFor="nome">Nome Completo</label>
+              <input
+                type="text"
+                name="nome"
+                id="nome"
+                placeholder="Evite Abreviações..."
+              />
+            </div>
+            <div className="finalizar-label-input">
+              <label htmlFor="eMail">E-mail</label>
+              <input
+                type="email"
+                name="eMail"
+                id="eMail"
+                placeholder="usuario@teste.com"
+              />
+            </div>
+            <div className="finalizar-label-input">
+              <label htmlFor="telefone">Telefone Contato</label>
+              <input
+                type="tel"
+                name="telefone"
+                id="telefone"
+                placeholder="(00) 00000-0000"
+              />
+            </div>
+          </section>
+          <section>
+            <h2>Endereço de Entrega</h2>
+            <div className="finalizar-label-input">
+              <label htmlFor="cep">CEP</label>
+              <input
+                type="number"
+                name="cep"
+                id="cep"
+                placeholder="00000-000"
+              />
+            </div>
+            <div className="finalizar-label-input">
+              <label htmlFor="endereco">Endereço</label>
+              <input
+                type="text"
+                name="endereco"
+                id="endereco"
+                placeholder="Logradouro, nº..."
+              />
+            </div>
+            <div className="label-input-especial">
+              <div className="finalizar-label-input">
+                <label htmlFor="complemento">Complemento</label>
+                <input
+                  type="text"
+                  name="complemento"
+                  id="complemento"
+                  placeholder="Apto 000..."
+                />
+              </div>
+              <div className="finalizar-label-input">
+                <label htmlFor="bairro">Bairro</label>
+                <input
+                  type="text"
+                  name="bairro"
+                  id="bairro"
+                  placeholder="Meireles..."
+                />
+              </div>
+              <div className="finalizar-label-input">
+                <label htmlFor="cidade">Cidade</label>
+                <input
+                  type="text"
+                  name="cidade"
+                  id="cidade"
+                  placeholder="Fortaleza..."
+                />
+              </div>
+              <div className="finalizar-label-input">
+                <label htmlFor="estadoSelect">UF</label>
+                <select
+                  id="estadoSelect"
+                  name="estadoSelect"
+                  value={estadoSelecionado}
+                  onChange={handleChange}
+                >
+                  <option value="">Selecione um Estado</option>
+                  {estadosBrasil.map((estado, index) => (
+                    <option key={index} value={estado.abreviacao}>
+                      {estado.nome}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+          </section>
+          <section>
+            <h2 className="checkout-finalizar-titulo">Valores</h2>
+            <div className="checkout-finalizar">
+              <span>Total</span>
+              <strong>R$ 50,00</strong>
+              <button>Finalizar Pedido</button>
+            </div>
+          </section>
+        </form>
+      </main>
+
+      <section aria-label="Lista de Produtos Disponíveis"></section>
     </>
   );
 }
--- src/pages/historico/historico.jsx
+++ src/pages/historico/historico.jsx
@@ -1,1 +1,68 @@
+import "./historico.css";
+import Navbar from "../../components/navbar/navbar";
 
+export default function Historico() {
+
+  const pedidos = [
+    {id_pedido: 51214, dt: '14/01/2024', total: 150.00},
+    {id_pedido: 76458, dt: '14/01/2024', total: 350.00},
+    {id_pedido: 24367, dt: '14/01/2024', total: 55.00},
+    {id_pedido: 38572, dt: '14/01/2024', total: 12.00},
+    {id_pedido: 89552, dt: '14/01/2024', total: 70.00},
+    {id_pedido: 34566, dt: '14/01/2024', total: 45.00},
+  ]
+
+  return (
+    <>
+      <header>
+        <Navbar showMenu={true} />
+      </header>
+      <main>
+        <div class="container header-content">
+          <h1>Histórico de Pedidos</h1>
+          <ul class="lista-historico">
+            <li>
+              <div
+                className="lista-info"
+                role="listitem"
+                aria-labelledby="pedido-1"
+              >
+                <h2 id="pedido-1">Pedido: 51214</h2>
+                <div className="lista-info-p">
+                  <p aria-label="Data do pedido">02/12/2023</p>
+                  <p aria-label="Valor do pedido">R$ 18,90</p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div
+                className="lista-info"
+                role="listitem"
+                aria-labelledby="pedido-2"
+              >
+                <h2 id="pedido-2">Pedido: 77374</h2>
+                <div className="lista-info-p">
+                  <p aria-label="Data do pedido">02/12/2023</p>
+                  <p aria-label="Valor do pedido">R$ 20,90</p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div
+                className="lista-info"
+                role="listitem"
+                aria-labelledby="pedido-3"
+              >
+                <h2 id="pedido-3">Pedido: 97364</h2>
+                <div className="lista-info-p">
+                  <p aria-label="Data do pedido">02/12/2023</p>
+                  <p aria-label="Valor do pedido">R$ 120,90</p>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </main>
+    </>
+  );
+}
